8adfd7acd6a3457bd1ed31a2a4840e2197150134
ninochachua <92683978+ninochachua@users.noreply.github.com>
2022-06-04 16:28:23 +0300

    PZ_2, PZ_3_1,PZ_3_2

---
+++ С++++++/PZ_3_2/PZ_3_2/PZ_3_2.cpp
@@ -0,0 +1,32 @@
+﻿# include <iostream>
+# include <windows.h>
+# include <cmath>
+
+using namespace std;
+
+int main()
+
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int a, b, c, d;
+    int n;
+    cout << "Введите четыре целых числа, одно из которых отлично от трех других: " << endl;
+    cout << "Введите первое число a: ";
+    cin >> a;
+    cout << "Введите второе число b: ";
+    cin >> b;
+    cout << "Введите третье число c: ";
+    cin >> c;
+    cout << "Введите четвертое число d: ";
+    cin >> d;
+    if (a == b && b == c) n = 4;
+    else if (a == b && a == d) n = 3;
+    else if (a == c && a == d) n = 2;
+    else if (b == c && b == d) n = 1;
+    cout << "порядковый номер числа, отличного от остальных:  " << n << endl;
+
+    system("pause");
+    return 0;
+}
---
+++ С++++++/pz_2/pz_2/pz_2.cpp
@@ -0,0 +1,19 @@
+﻿# include <iostream>
+# include<Windows.h>
+
+int main() {
+    using namespace std;
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    
+    int K;
+    int number_day;
+
+    cout << "Введите число K от 1 до 365:  ";
+    cin >> K;
+    number_day = (K % 7) + 1;
+    cout << "Номер дня недели = " << number_day << endl;
+    return 0;
+}   
